7c4f94d37dcf001d39c5f14b51f506ef77bf1bf6
Alex Bleasdale <ableasdale@marklogic.com>
2016-09-06 10:14:23 +0100

    Playing local file through mp3 player

--- src/main/xquery/assets/init.js
+++ src/main/xquery/assets/init.js
@@ -6,20 +6,25 @@ $(document).ready(function () {
         cssSelectorAncestor: "#jp_container_1"
     }, [
         {
-            title: "<span class='item-artist'>Rihanna - </span><span class='item-song'>Only Girl</span><img class='item-image' src='images/temp/music-player-4.jpg' alt='' />",
-            mp3: "http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
-            oga: "http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
-        },
-        {
-            title: "<span class='item-artist'>Yaki Da - </span><span class='item-song'>Dancing</span><img class='item-image' src='images/temp/music-player-5.jpg' alt='' />",
-            mp3: "http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
-            oga: "http://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
-        },
-        {
-            title: "<span class='item-artist'>Pandora - </span><span class='item-song'>Sands of Time</span><img class='item-image' src='images/temp/music-player-6.jpg' alt='' />",
-            mp3: "http://www.jplayer.org/audio/mp3/Miaow-07-Bubble.mp3",
-            oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+            title: "<span class='item-artist'>Olafur Arnalds - </span><span class='item-song'>Study for Generative Piano (i)</span><img class='item-image' src='/assets/images/sample-mp3-img.jpg' alt='' />",
+            mp3: "/assets/01 Study for Generative Piano (i).mp3"
         }
+
+        /*        {
+         title: "<span class='item-artist'>Rihanna - </span><span class='item-song'>Only Girl</span><img class='item-image' src='images/temp/music-player-4.jpg' alt='' />",
+         mp3: "http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+         oga: "http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+         },
+         {
+         title: "<span class='item-artist'>Yaki Da - </span><span class='item-song'>Dancing</span><img class='item-image' src='images/temp/music-player-5.jpg' alt='' />",
+         mp3: "http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
+         oga: "http://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
+         },
+         {
+         title: "<span class='item-artist'>Pandora - </span><span class='item-song'>Sands of Time</span><img class='item-image' src='images/temp/music-player-6.jpg' alt='' />",
+         mp3: "http://www.jplayer.org/audio/mp3/Miaow-07-Bubble.mp3",
+         oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+         } */
     ], {
         swfPath: "js",
         supplied: "oga, mp3",
